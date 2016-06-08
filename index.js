cefdee159be402487ece2bcedd090a423317b479
wrleskovec <russell.leskovec@gmail.com>
2016-06-08 17:43:37 -0500

    fixed prefix directory

--- index.js
+++ index.js
@@ -6,13 +6,14 @@ var argv = process.argv.slice(2);
 var path = require('path');
 
 if (['save', 'load', 'remove'].indexOf(argv[0]) > -1 && argv[1]) {
-  var dirName = argv[2] ? argv[2] : '.';
+  var dirName = argv[2] ? path.resolve(argv[2]) : '.';
+  console.log(dirName);
   var lintModules;
   var eslintrcConfig;
   var savedConfigs;
 
   if (argv[0] === 'save') {
-    fs.readdirAsync(path.join(dirName, '/node_modules'))
+    fs.readdirAsync(path.join(dirName, 'node_modules'))
       // find eslint modules in project
       .then(function(stats) {
         lintModules = stats.filter(function(d) {
@@ -30,7 +31,7 @@ if (['save', 'load', 'remove'].indexOf(argv[0]) > -1 && argv[1]) {
         var eslintrcFile = files.find(function(file) {
           return /\.eslintrc/.test(file);
         });
-        return fs.readFileAsync(path.join(dirName, '/', eslintrcFile));
+        return fs.readFileAsync(path.join(dirName, eslintrcFile));
       })
       // read project .eslintrc* and write to configs file
       .then(function(file) {
@@ -52,14 +53,14 @@ if (['save', 'load', 'remove'].indexOf(argv[0]) > -1 && argv[1]) {
       .then(function(file) {
         savedConfigs = JSON.parse(file);
         if (savedConfigs[argv[1]]) {
-          return execAsync('npm i -D ' + savedConfigs[argv[1]].modules.join(' '));
+          return execAsync('npm -C '+ dirName + ' i -D ' + savedConfigs[argv[1]].modules.join(' '));
         }
         return Promise.reject('Config not found');
       })
       .then(function(stdout) {
         console.log(stdout);
         var stringOfJSON = JSON.stringify(savedConfigs[argv[1]].eslintrc, null, 2);
-        return fs.writeFileAsync(path.join(dirName, '/.eslintrc.json'), stringOfJSON);
+        return fs.writeFileAsync(path.join(dirName, '.eslintrc.json'), stringOfJSON);
       })
       .then(function(err) {
         if (err) throw err;
@@ -72,13 +73,13 @@ if (['save', 'load', 'remove'].indexOf(argv[0]) > -1 && argv[1]) {
       .then(function(file) {
         savedConfigs = JSON.parse(file);
         if (savedConfigs[argv[1]]) {
-          return execAsync('npm un -D ' + savedConfigs[argv[1]].modules.join(' '));
+          return execAsync('npm -C '+ dirName + ' i -D ' + savedConfigs[argv[1]].modules.join(' '));
         }
         return Promise.reject('Config not found');
       })
       .then(function(stdout) {
         console.log(stdout);
-        return fs.writeFileAsync(path.join(dirName, '/.eslintrc.json'), '{}');
+        return fs.writeFileAsync(path.join(dirName, '.eslintrc.json'), '{}');
       })
       .then(function(err) {
         if (err) throw err;
